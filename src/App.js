d33982386f469db4e9271866d4cd1db5d1272bc5
Vino-Bastin <vino105264@gmail.com>
2023-03-17 19:52:01 +0530

    Mui package added and initial table setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import React from "react";
+import { ThemeProvider, createTheme, CssBaseline, Box } from "@mui/material";
+
+import Table from "./Table";
 
 function App() {
-  return <></>;
+  const theme = createTheme({});
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box
+          display="flex"
+          flex={1}
+          height="100vh"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Table />
+        </Box>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
